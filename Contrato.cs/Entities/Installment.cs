b540ca66fed081c1785186e63a8d1b66e1a8c1ff
Randolfo Machado <talktorandolph@live.com>
2019-07-29 21:08:41 -0300

    Criacao da pasta entities e das classes Contract.cs, Installment.cs

---
+++ Contrato.cs/Entities/Installment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Contrato.cs.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+    }
+}
